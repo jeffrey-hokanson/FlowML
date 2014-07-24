d2a6ac7f9cb6543968c4cd02981b0d9bb337a93d
Jeffrey Hokanson (loki) <jeffrey@hokanson.us>
2014-07-23 19:53:51 -0500

    Now pulls in all functions under flowml

--- flowml/__init__.py
+++ flowml/__init__.py
@@ -1,1 +1,8 @@
-__all__ = ['fcs', 'flowdata']
+#__all__ = ['fcs', 'flowdata']
+
+from flowdata import FlowData
+from analysis import * 
+
+# Disabled temporarly
+#import mpld3
+#mpld3.enable_notebook()
