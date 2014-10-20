06722f7625dc89dc10d57656f077b5e579cfd121
Jeffrey Hokanson (loki) <jeffrey@hokanson.us>
2014-10-20 16:34:43 -0500

    Changed name from Thresholds to Threshold

--- flowml/__init__.py
+++ flowml/__init__.py
@@ -1,6 +1,7 @@
 #__all__ = ['fcs', 'flowdata']
 
 from flowdata import FlowData
+from flowdata_util import *
 from analysis import * 
 from bead import *
 import arcsinh_transform
@@ -11,8 +12,7 @@ import spade
 
 import kde
 
-from threshold_gate import threshold_gate
-
+from threshold_gate import threshold_gate, Threshold
 # Disabled temporarly
 #import mpld3
 #mpld3.enable_notebook()
